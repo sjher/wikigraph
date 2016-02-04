99ef42233b66bf7fb922c466a45f9808395c7353
sjher <sven.hermann@gmail.com>
2016-02-04 15:54:48 -0500

    fix old ugly vim indents

--- spark/pagerank/src/main/scala/cleanup.scala
+++ spark/pagerank/src/main/scala/cleanup.scala
@@ -8,80 +8,80 @@ import org.apache.spark.SparkConf
 object cleanWikiData {
 	def main(args: Array[String]) {
 		val sparkConf = new SparkConf()
-		.setAppName("cleanWikiData")
+			.setAppName("cleanWikiData")
 		val sc = new SparkContext(sparkConf)
 
 		def toLong(s: String): Long = {
-				try {
-						s.toLong
-				} catch {
-						case e: Exception => -1
-				}
+			try {
+				s.toLong
+			} catch {
+				case e: Exception => -1
+			}
 		}
 
 		def toInt(s: String): Int = {
-  			try {
-    				s.toInt
-  			} catch {
-    				case e: Exception => -1
-  			}
+			try {
+				s.toInt
+			} catch {
+				case e: Exception => -1
+			}
 		}
 
 		val linksFile = sc.textFile("/wikipedia/pagelinks.txt")
 
 		val links = linksFile.map {
-				line => line.split(" ")
+			line => line.split(" ")
 		}.filter {
-				case linkTuple => (linkTuple.length == 4)
+			case linkTuple => (linkTuple.length == 4)
 		}.map {
-				case (id,ns1,title,ns2) => (toLong(id),toInt(ns1),title,toInt(ns2))
+			case (id,ns1,title,ns2) => (toLong(id),toInt(ns1),title,toInt(ns2))
 		}.filter {
-				case (id,ns1,_,ns2) => ((id != -1) && (ns1 != -1) && (ns2 != -1))
+			case (id,ns1,_,ns2) => ((id != -1) && (ns1 != -1) && (ns2 != -1))
 		}.filter {
-				case (_, ns1, _, ns2) => (ns1 == 0 && ns2 == 0)
+			case (_, ns1, _, ns2) => (ns1 == 0 && ns2 == 0)
 		}
 
 		val pagesFile = sc.textFile("/wikipedia/en-pages.txt")
 
 		val pages = pagesFile.map {
-			 	line => line.split(" ")
+			line => line.split(" ")
 		}.filter {
-				case pageTuple => (pageTuple.length == 3)
+			case pageTuple => (pageTuple.length == 3)
 		}.map { line =>
-				case (id,ns,title) => (toLong(id),toInt(ns),title)
+			case (id,ns,title) => (toLong(id),toInt(ns),title)
 		}.filter {
-				case (id,ns,title) => ((id != -1) && (ns != -1))
+			case (id,ns,title) => ((id != -1) && (ns != -1))
 		}.filter {
-				case (_, ns, _) => ns == 0
+			case (_, ns, _) => ns == 0
 		}.map {
-				case (id, _, title) => (id, title)
+			case (id, _, title) => (id, title)
 		}.persist(org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK)
-		
+
 		val pagesReverse = pages.map {
-				case (id, title) => (title, id)
+			case (id, title) => (title, id)
 		}
 
 		val linksReverse = links.map {
-				case (fromPageId, _, toPageTitle, _) => (toPageTitle, fromPageId)
+			case (fromPageId, _, toPageTitle, _) => (toPageTitle, fromPageId)
 		}
 
 		//remove edges with destination title that don't have corresponding pageId
 		val validDestinationsOnly = pagesReverse.join(linksReverse).map {
-				case (name, (toPageId, fromPageId)) => (fromPageId, toPageId)
+			case (name, (toPageId, fromPageId)) => (fromPageId, toPageId)
 		}
 
 		//remove edges with source pageId that don't have corresponding page title
 		val edges = pages.join(validDestinationsOnly).map {
-				case (fromPageId, (title, toPageId )) => (fromPageId, toPageId)
+			case (fromPageId, (title, toPageId )) => (fromPageId, toPageId)
 		}
 
 		val edgesForPrint = edges.map {
-				case (fromId, toId) => Array(fromId, toId).mkString(",")
+			case (fromId, toId) => Array(fromId, toId).mkString(",")
 		}
 		edgesForPrint.saveAsTextFile("/wikipedia/en/cleanedLinks")
-                
+
 		val ranksForPrint = pages.map {
-				case (id, title) => Array(id, title).mkString(",")
+			case (id, title) => Array(id, title).mkString(",")
 		}
 		ranksForPrint.saveAsTextFile("/wikipedia/en/cleanedPages")
 	}
--- spark/pagerank/src/main/scala/pagerankfast.scala
+++ spark/pagerank/src/main/scala/pagerankfast.scala
@@ -8,31 +8,31 @@ import org.apache.spark.SparkConf
 object wikiPageRank {
 	def main(args: Array[String]) {
 		val sparkConf = new SparkConf()
-		.setAppName("wikiPageRank")
+			.setAppName("wikiPageRank")
 		val sc = new SparkContext(sparkConf)
 
 		val linkFiles = sc.textFile("/wikipedia/en/cleanedLinks/part-*")
 		val linkLines = sc.union(linkFiles)
-		
+
 		val links = linkLines.map {
-				line => line.split(",")
+			line => line.split(",")
 		}.filter {
-				case line => (line.length == 2)
+			case line => (line.length == 2)
 		}.map {
-				line => (line(0).toLong, line(1).toLong)
+			line => (line(0).toLong, line(1).toLong)
 		}
-		
+
 		val pageFiles = sc.textFile("/wikipedia/en/cleanedPages/part-*")
 		val pageLines = sc.union(pageFiles)
-		
+
 		val pages = pageLines.map{
-				line => line.split(",")
+			line => line.split(",")
 		}.filter {
-				case line => (line.length == 2)
+			case line => (line.length == 2)
 		}.map {
-				line => (line(0).toLong, line(1))
+			line => (line(0).toLong, line(1))
 		}
-		
+
 		val graph = Graph.fromEdgeTuples(edges,1)
 
 		//Run PageRank
@@ -41,7 +41,7 @@ object wikiPageRank {
 		val ranksByVertex = pages.join(ranks.vertices)
 
 		val ranksForPrint = ranksByVertex.map {
-				case (id, (title, rank)) => Array(id, title, rank).mkString(",")
+			case (id, (title, rank)) => Array(id, title, rank).mkString(",")
 		}
 		ranksForPrint.saveAsTextFile("/wikipedia/en/wikiPageRanks")
 	}
