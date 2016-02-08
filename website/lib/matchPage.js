9e8d2e8f7aa23db281b6b5340fd4ea974e869b2b
sjher <sven.hermann@gmail.com>
2016-02-08 09:56:11 -0500

    fix label convention to new server

--- website/lib/matchPage.js
+++ website/lib/matchPage.js
@@ -2,7 +2,7 @@ var cypherQuery = require("./cypherQuery.js");
 var _ = require("lodash");
 
 function matchPage(pageBeginning, cb) {
-    var query = 'MATCH (p:PAGE) WHERE p.title STARTS WITH "' + pageBeginning + '" RETURN p';
+    var query = 'MATCH (p:Page) WHERE p.title STARTS WITH "' + pageBeginning + '" RETURN p';
 
     cypherQuery(query, function(error, body) {
         if (error) {
